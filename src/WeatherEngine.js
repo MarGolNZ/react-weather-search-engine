b6a6531b47b7d121770f2efd019b9a9fb39859fc
MarGolNZ <54025171+MarGolNZ@users.noreply.github.com>
2021-05-08 15:40:17 +1000

    Add weather-engine.js component

---
+++ src/WeatherEngine.js
@@ -0,0 +1,17 @@
+import "./WeatherEngine.css";
+import React from "react";
+
+export default function WeatherEngine() {
+
+    return(
+
+        <div className = "WeatherEngine">
+
+<h1>React Weather Engine</h1>
+
+
+
+
+        </div>
+    ) 
+}
